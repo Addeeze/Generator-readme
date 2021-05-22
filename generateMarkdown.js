d7d5a90358b65613ade01014a8f3eb7496526f0f
Nicholas.Veen <Nicholas.veen@gmail.com>
2021-05-22 10:50:33 -0700

    push to github

---
+++ generateMarkdown.js
@@ -0,0 +1,44 @@
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `## ${data.title}
+  
+  # Table of Contents 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Test](#tests)
+  * [Questions](#questions)
+
+  ## Description
+  ${data.description}
+  
+  # Installation
+  ${data.installation}
+  
+  # Usage 
+  ${data.usage}
+  
+  # Licence
+  ![License](https://img.shields.io/badge/License-${data.license}-blue.svg)
+  
+  for more information about this license click here [license](https://opensource.org/licenses/${data.license})
+  
+  # Constributions
+  ${data.contributions}
+ 
+  # Tests
+  ${data.tests}
+  
+  ## Questions
+  ${data.questions}
+     
+  Github: ${data.github}
+  E-mail: ${data.email}
+  
+`;
+}
+
+module.exports = generateMarkdown;
